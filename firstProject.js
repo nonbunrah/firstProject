3837bd7e87fd8222d2035bc50666ae9d7e38ae7e
RJ Bamrah <RJ@RJs-MacBook-Pro.local>
2019-11-05 13:49:18 -0800

    buttons work

--- firstProject.js
+++ firstProject.js
@@ -1,3 +1,30 @@
-document.createElement("button")
-button.innerHTML("hey")
-body.append(button)
+//Submit button
+
+let scoreArray = []
+let score = 0;
+
+let button = document.querySelector(".button");
+
+button.addEventListener("click", alertScreen = () => {
+	for (input of answerInputs) {
+	if(input.value == "VW" && input.checked) {
+		scoreArray.push(1)
+	}
+}	
+let total = scoreArray.reduce((a,b) => a + b)
+console.log(total)
+
+alert("You got " + total + " out of 10 correct!")
+
+console.log(answerInputs)
+});
+
+//Redo Quiz button
+let refresh = document.querySelector(".refresh")
+
+refresh.addEventListener("click", reload = () => {
+	location.reload(true),
+	scroll(0,0)
+});
+
+let answerInputs = document.querySelectorAll("input[type = 'radio']")
