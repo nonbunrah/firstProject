22d55d6d83774c5c9ac794e1116081b8515eac5c
RJ Bamrah <RJ@RJs-MacBook-Pro.local>
2019-11-07 09:47:33 -0800

    just a tiny bit left

--- firstProject.js
+++ firstProject.js
@@ -1,21 +1,40 @@
+//image click
+
+let imageClick = document.querySelector("img")
+
+//imageClick.addEventListener("click", )
+	let clicked = () => {
+	console.log('hey!')
+}
+
+console.log(imageClick)
+
 //Submit button
 
 let scoreArray = []
-let score = 0;
 
 let button = document.querySelector(".button");
 
 button.addEventListener("click", alertScreen = () => {
+	scoreArray.length = 0
 	for (input of answerInputs) {
 	if(input.value == "VW" && input.checked) {
 		scoreArray.push(1)
 	}
 }	
+
 let total = scoreArray.reduce((a,b) => a + b)
+if (total <= 5) {
+	alert("You suck! You got " + total + " out of 10 correct!")
+} else if (total > 5 && total <= 8) {
+	alert("Not bad! You got " + total + " out of 10 correct!")
+} else if (total === 9) {
+	alert("Nice job! You got " + total + " out of 10 correct!")
+} else if (total === 10) {
+	alert("Wow! Perfect Score! 10 out of 10!")
+}
+
 console.log(total)
-
-alert("You got " + total + " out of 10 correct!")
-
 console.log(answerInputs)
 });
 
